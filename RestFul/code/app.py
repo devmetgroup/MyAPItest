6dfb7c8a8215de95a98e54665c4b2558dd9780c3
DeimerM <moralesdei@protonmail.com>
2021-08-20 16:47:29 -0500

    Creating a log in system

--- RestFul/code/app.py
+++ RestFul/code/app.py
@@ -1,13 +1,35 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = 'jose'
 api = Api(app)
 
-class Student(Resource):
+jwt = JWT(app, authenticate, identity) # creates an endpoint called /auth
+
+items = []
+
+class Item(Resource):
+    @jwt_required()
     def get(self, name):
-        return {'student': name}
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': item}, 200 if item is not None else 404
+    
+    def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return {'message': "An item with name '{}' already exists.".format(name)}, 404
+        request_data = request.get_json()
+        item = {'name': name, 'price': request_data['price']}
+        items.append(item)
+        return item, 201
 
-api.add_resource(Student, '/student/<string:name>')
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
+    
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
 
-app.run(port=5000)
+app.run(port=5000, debug=True)
