411da120a53dba9110ac913ba3575ee8eb52ca5e
DeimerM <moralesdei@protonmail.com>
2021-08-21 17:26:06 -0500

    Parsing json payloads to retrieve the options that we need only

--- RestFul/code/app.py
+++ RestFul/code/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 from security import authenticate, identity
 
@@ -12,6 +12,12 @@ jwt = JWT(app, authenticate, identity) # creates an endpoint called /auth
 items = []
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required=True,
+        help='This field cannot be left blank!')
+        
     @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
@@ -19,11 +25,26 @@ class Item(Resource):
     
     def post(self, name):
         if next(filter(lambda x: x['name'] == name, items), None):
-            return {'message': "An item with name '{}' already exists.".format(name)}, 404
-        request_data = request.get_json()
+            return {'message': "An item with name '{}' already exists.".format(name)}, 404  
+        request_data = Item.parser.parse_args()
         item = {'name': name, 'price': request_data['price']}
         items.append(item)
         return item, 201
+    
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'mesage': 'Item deleted'}
+    
+    def put(self, name):
+        request_data = Item.parser.parse_args()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': request_data['price']}
+            items.append(item)
+        else:
+            item.update(request_data)
+        return item
 
 class ItemList(Resource):
     def get(self):
