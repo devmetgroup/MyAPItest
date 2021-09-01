c4a62e3029edf15f5549596e90f6fa09a4de7c77
DeimerM <moralesdei@protonmail.com>
2021-09-01 10:41:25 -0500

    Creating jwt callbacks and log out resources, also added fresh and non-fresh token

--- RestFul/code/app.py
+++ RestFul/code/app.py
@@ -1,15 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
-from resources.user import UserRegister, User, UserLogin
+from resources.user import UserRegister, User, UserLogin, UserLogout, TokenRefresh
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
+from blacklist import BLACKLIST
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
-app.secret_key = 'jose'
+app.config['JWT_SECRET_KEY'] = 'jose'
 api = Api(app)
 
 @app.before_first_request
@@ -18,11 +19,50 @@ def create_tables():
     
 jwt = JWTManager(app)
 
+@jwt.token_in_blocklist_loader
+def check_if_token_in_blacklist(jwt_header, jwt_data):
+    return jwt_data['jti'] in BLACKLIST
+
 @jwt.additional_claims_loader
 def add_claims_to_jwt(identity):
     if identity == 1:
         return {'is_admin': True}
     return {'is_admin': False}
+
+@jwt.expired_token_loader
+def expired_token_callback(error_header, error_data):
+    return jsonify({
+        'description': 'The token has expired.',
+        'error': 'token_expired.'
+    }), 401
+
+@jwt.invalid_token_loader
+def invalid_token_callback(error):
+    return jsonify({
+        'description': 'Signature verification failed.',
+        'error': 'invalid_expired.'
+    }), 401
+
+@jwt.unauthorized_loader
+def missing_token_callback(error):
+    return jsonify({
+        'description': 'Request does not contain an access token.',
+        'error': 'unauthorized_expired.'
+    }), 401
+
+@jwt.needs_fresh_token_loader
+def token_not_fresh_callback():
+    return jsonify({
+        'description': 'The token is not fresh.',
+        'error': 'fresh_token_required.'
+    }), 401
+
+@jwt.revoked_token_loader
+def revoked_token_callback(error_header, error_data):
+    return jsonify({
+        'description': 'The token has been revoked.',
+        'error': 'token_revoked.'
+    }), 401
 
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>')
@@ -30,7 +70,9 @@ api.add_resource(ItemList, '/items')
 api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 api.add_resource(User, '/user/<int:user_id>')
-api.add_resource(UserLogin, '/auth')
+api.add_resource(UserLogin, '/login')
+api.add_resource(UserLogout, '/logout')
+api.add_resource(TokenRefresh, '/refresh')
 
 if __name__ == "__main__":
     from db import db
