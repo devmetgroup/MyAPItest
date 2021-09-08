7adb517c1737abbbd5d082ff674419f098be22c9
DeimerM <moralesdei@protonmail.com>
2021-09-08 10:51:30 -0500

    adding typing

--- RestFul/code/libs/strings.py
+++ RestFul/code/libs/strings.py
@@ -16,7 +16,7 @@ def refresh():
     with open(f"strings/{default_locale}.json") as f:
         cached_strings = json.load(f)
 
-def gettext(name):
+def gettext(name: str) -> str:
     return cached_strings[name]
 
 refresh()
